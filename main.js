835d7873205a7b80fa8aed3d5cf011bcdd5ca68d
deoxce <deoxceofficial@gmail.com>
2024-10-04 22:18:50 +0500

    init

---
+++ main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const ipc = ipcMain;
+
+function createWindow() {
+    const window = new BrowserWindow({
+        titleBarStyle: 'hidden',
+        icon: "app.ico",
+        width: 1280,
+        height: 720,
+        minWidth: 940,
+        minHeight: 560,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    window.loadFile('app/index.html');
+
+    ipc.on('closeApp', () => {
+        window.close();
+    });
+
+    ipc.on('minimizeApp', () => {
+        window.minimize();
+    });
+
+    ipc.on('maximizeRestoreApp', () => {
+        if(window.isMaximized()) {
+            window.restore();
+        } else {
+            window.maximize();
+        }
+    });
+}
+
+app.on('ready', () => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (!process.platform === 'darwin') {
+        app.quit();
+    }
+});
